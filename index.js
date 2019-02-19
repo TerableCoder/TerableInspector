9d9292986ff54b8411431fb2db4bfe7017da4af3
Tera Coder <TerableCoder@gmail.com>
2019-02-19 18:00:54 -0500

    add inspect by name

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ module.exports = function TerableLockons(mod){
 	const command = mod.command || mod.require.command;
 	
 	command.add(['terai', 'tinspector', 'terainspector', 'terableinspector'], (p1)=> {
-		if(isNaN(p1)){
+		if(!isNaN(p1)){
+			mod.toServer('C_REQUEST_USER_PAPERDOLL_INFO_WITH_GAMEID', 1, {id: Number(p1)});
+		} else if(typeof p1 === 'string' || p1 instanceof String){
+			mod.toServer('C_REQUEST_USER_PAPERDOLL_INFO', 1, {name: p1});
+	  	} else{
 			command.message(p1 + "is not a number");
-			return;
-		} else mod.toServer('C_REQUEST_USER_PAPERDOLL_INFO_WITH_GAMEID', 1, {id: Number(p1)});
+		}
 	});
 	
 	
